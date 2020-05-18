e6ff82e9c4ffd9a99eac19e736093b619f60e47c
Nikos Gkountios <33179072+NGBoost@users.noreply.github.com>
2020-05-18 01:18:52 +0100

    Create solve.py

---
+++ solve.py
@@ -0,0 +1,112 @@
+# This script contains helper functions to solve the Sudoku puzzle.
+
+import numpy as np
+
+def is_valid(grid, num, pos):
+    """
+    Determines whether it will be legal to assign num to the given row,col.
+
+    Args:
+        grid: The Sudoku board. Format: 2D nested lists of integers.
+        num: The number we want to test if it is valid in the given position.
+        pos: The position.
+
+    Returns:
+        A boolean which indicates whether it will be legal to assign num to given row,column location.
+    """
+    # Check the row.
+    for i in range(len(grid[0])):
+        if grid[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check the column.
+    for i in range(len(grid)):
+        if grid[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check the box.
+    box_x = (pos[0] // 3) * 3
+    box_y = (pos[1] // 3) * 3
+    for i in range(box_x, box_x + 3):
+        for j in range(box_y, box_y + 3):
+            if grid[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+def empty_square(grid):
+    """ Finds an empty square in the Sudoku board and return the position.
+
+    Args:
+        grid: The Sudoku board. Format: 2D nested lists of integers.
+
+    Returns:
+        Either a tuple representing the position of the empty square. (row,column).
+        Or None if there is not an empty square.
+    """
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 0:
+                return (i, j)
+
+    return None
+
+def solve(grid):
+    """
+    Solve the Sudoku puzzle using recursion and backtracking.
+    Takes a partially unassigned filled-in grid and attempts to find a solution.
+
+    Args:
+        grid: The Sudoku grid. Format: 2D nested lists of integers.
+
+    Returns:
+        True if there is a feasible solution, False if not.
+        N.B. If there is a feasible solution, after the execution of the function we can print the Sudoku solution.
+    """
+    emp_sqr = empty_square(grid)
+
+    if emp_sqr is None:
+        print("-----------------------")
+        print('Solved!')
+        print('Solution:')
+        print_board(grid)
+        return True
+    else:
+        row = emp_sqr[0]
+        col = emp_sqr[1]
+
+    for i in range(1,10):
+        if is_valid(grid, i, (row, col)):
+            grid[row][col] = i
+
+            # recursion
+            if solve(grid):
+                return True
+
+            # trigger backtracking
+            grid[row][col] = 0
+
+    return False
+
+def print_board(grid):
+    """ Print the Sudoku board.
+
+    Args:
+        grid: The Sudoku board. Format: 2D nested lists of integers.
+
+    Returns:
+        A user friendly visual representation of the Sudoku board.
+    """
+    for i in range(len(grid)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - -")
+
+        for j in range(len(grid[0])):
+            if j % 3 == 0 and j != 0:
+                print("|", end="")
+
+            if j == 8:
+                print(grid[i][j])
+            else:
+                print(str(grid[i][j]) + " ", end="")
+        
